6e9455cd797d518e65dc9a16283a2e9d77a87fc9
novalramdhani <novalfitrahramdhani@gmail.com>
2022-02-11 22:53:39 +0700

    add factory and seeder for article

---
+++ backend/database/factories/ArticleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'slug' => str($this->faker->sentence)->slug(),
+            'thumbnail' => $this->faker->image,
+            'content' => $this->faker->paragraph(40)
+        ];
+    }
+}
--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Article::factory(50)->create();
     }
 }
