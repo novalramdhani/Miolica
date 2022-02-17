a08114e0af5ef465c0e18a9ba753092e779078e6
andrianfaa <andrianfadhilla@gmail.com>
2022-02-17 13:38:42 +0700

    update: change from javascript to typescript, added [Redux](https://redux.js.org/) for state management, [TailwindCSS](https://tailwindcss.com/) v3 for styling, [ESLint](https://eslint.org/) and [Prettier](https://prettier.io/) for code formatting and code writing consistency

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// Pages
+import HomePage from 'pages/Home';
+
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+    );
+}
