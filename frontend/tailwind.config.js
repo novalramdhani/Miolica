506022ee5923a95998411881cf35dc0fd0c4aeab
andrianfaa <andrianfadhilla@gmail.com>
2022-02-27 23:50:48 +0700

    update: added syne font and added loading bar on top when page loads

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+      syne: ['Syne', 'sans-serif'],
+    },
   },
   plugins: [],
 };
