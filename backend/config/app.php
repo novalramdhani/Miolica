b85dfe35de3bfbcdbbfca064d91932805e59d6ce
novalramdhani <novalfitrahramdhani@gmail.com>
2022-03-01 21:00:01 +0700

    setup mongodb service package

--- backend/config/app.php
+++ backend/config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
 
         /*
          * Application Service Providers...
