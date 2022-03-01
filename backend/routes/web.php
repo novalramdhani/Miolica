ca2e664cf0cb05126897f99a40c90d55f8b6f0b0
novalramdhani <novalfitrahramdhani@gmail.com>
2022-03-01 20:16:43 +0700

    refactoring

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return response()->json([
         'Laravel' => app()->version(),
-        'Environment' => config('app.env')
+        'environment' => config('app.env')
     ]);
 });
