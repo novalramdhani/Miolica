14880135b16c773b591d293326a620a3cc886edc
andrianfaa <andrianfadhilla@gmail.com>
2022-03-03 04:08:32 +0700

    testing redux createApi

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import type { PreloadedState } from '@reduxjs/toolkit';
 
 import { authSlice } from 'features/auth';
+import { TestApiServices } from 'services';
 import AppSlice from './app.slice';
 
 const rootReducer = combineReducers({
   // ... other reducers
   app: AppSlice.reducer,
   auth: authSlice.reducer,
+  [TestApiServices.reducerPath]: TestApiServices.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
@@ -17,6 +19,7 @@ export const setupStore = (preloadedState?: PreloadedState<RootState>) => config
   middleware: (getDefaultMiddleware: () => any) => [
     ...getDefaultMiddleware(),
     // ... other middleware
+    TestApiServices.middleware,
   ],
   preloadedState,
 });
