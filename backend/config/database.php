6f2a87438f720669eb18b999ff7e6f0a52d02ee7
novalramdhani <novalfitrahramdhani@gmail.com>
2022-03-04 19:11:29 +0700

    add prefix config mongodb in database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -91,6 +91,27 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', 'secret'),
+            'options' => [
+                // here you can pass more settings to the Mongo Driver Manager
+                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
+
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
+            ],
+        ],
+
+        // 'mongodb' => [
+        //     'driver' => 'mongodb',
+        //     'dsn' => env('DB_DSN'),
+        //     'database' => env('DB_DATABASE', 'forge'),
+        // ],
+
     ],
 
     /*
