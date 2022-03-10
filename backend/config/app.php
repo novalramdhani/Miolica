a3fa63af7bb2079e352df2d8e344697decf95d45
novalramdhani <novalfitrahramdhani@gmail.com>
2022-03-10 20:32:43 +0700

    setup

--- backend/config/app.php
+++ backend/config/app.php
@@ -168,6 +168,7 @@ return [
          * Package Service Providers...
          */
         Jenssegers\Mongodb\MongodbServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -192,7 +193,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // ...
+        'Image' => Intervention\Image\Facades\Image::class
     ])->toArray(),
 
 ];
